f0a271ee8a525266e29fcbcba61cdde75ae3f94e
Bruce Huang <mailbruced@gmail.com>
2026-06-08 21:40:40 +0800

    fix: make __main__.py resilient to missing main module

--- apotheosession/__main__.py
+++ apotheosession/__main__.py
@@ -1,3 +1,9 @@
-from .main import main
+import sys
 
-main()
+if __name__ == "__main__":
+    try:
+        from .main import main
+    except ImportError:
+        print("apotheosession.main not yet implemented", file=sys.stderr)
+        sys.exit(1)
+    main()
